09cdcb146b49af441a82310229e75e0ecf1d2ff1
Hemudi <ksum1205@naver.com>
2024-07-24 22:15:26 +0900

    :lipstick: Theme color 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 const Button = () => {
-  return <button className="p-2 rounded-md bg-purple-500 text-white">테스트 버튼</button>;
+  return <button className="p-2 rounded-md bg-primary-400 text-white">테스트 버튼</button>;
 };
 
 export default Button;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        black: '#121212',
+        error: '#FF2C20',
+        kakao: '#FEE500',
+        naver: '#03C75A',
+        primary: {
+          50: '#FEEFEF',
+          100: '#FEDFDF',
+          200: '#FDBEBE',
+          300: '#FB9E9E',
+          400: '#FA7D7D',
+          500: '#F95D5D',
+          600: '#D04B4B',
+        },
+        gray: {
+          50: '#F6F6F6',
+          100: '#EDEDED',
+          200: '#DBDBDC',
+          300: '#C9C9CA',
+          400: '#B7B7B9',
+          500: '#A5A5A7',
+          600: '#888889',
+          700: '#888889',
+          800: '#888889',
+          900: '#888889',
+        },
+      },
+    },
   },
   plugins: [],
 };
