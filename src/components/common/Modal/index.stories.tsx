092262c871cd480738dfe2ba962f884acc05849c
Hemudi <ksum1205@naver.com>
2024-07-25 20:55:13 +0900

    :sparkles: Modal 컴포넌트 구현

---
+++ src/components/common/Modal/index.stories.tsx
@@ -0,0 +1,26 @@
+import Modal from '@components/common/Modal';
+import Button from '@components/common/Button';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Modal',
+  component: Modal,
+  args: {},
+  tags: ['autodocs'],
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: '로그아웃 하시겠습니까?',
+    controls: (
+      <div className="flex h-fit w-full gap-2">
+        <Button variant="line">취소하기</Button>
+        <Button>로그아웃</Button>
+      </div>
+    ),
+  },
+};
