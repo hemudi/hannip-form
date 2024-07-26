358625e6a57b5cb85a03fbbf2602893209b1208d
Hemudi <ksum1205@naver.com>
2024-07-27 06:14:17 +0900

    :lipstick: Loading 페이지 마크업

---
+++ src/app/loading/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+const ERROR_IMAGE_URL = 'assets/images/loading.svg';
+
+const ResultLoading = () => {
+  return (
+    <>
+      <div className="flex h-full w-full select-none flex-col items-center justify-center gap-6 p-10">
+        <Image
+          className="h-auto w-full animate-bounce"
+          width="0"
+          height="0"
+          alt="error_image"
+          src={ERROR_IMAGE_URL}
+        />
+        <div className="text-h4 font-bold">스크립트가 구워지고 있어요</div>
+      </div>
+    </>
+  );
+};
+
+export default ResultLoading;
