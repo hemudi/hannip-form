5a0d6a7ba6bc1359f20f88f538c6d5c84c818422
Hemudi <ksum1205@naver.com>
2024-07-28 15:41:47 +0900

    :sparkles: 로그인 막음

--- src/components/Page/Home/LoginButton/index.tsx
+++ src/components/Page/Home/LoginButton/index.tsx
@@ -29,12 +29,10 @@ const LoginButton = ({ type }: LoginButtonProps) => {
   return (
     <Link
       onClick={() => {
-        if (type === 'naver') {
-          toast.success('네이버 로그인은 업데이트 예정 중입니다!');
-          return;
-        }
+        toast.success('현재 로그인은 업데이트 예정 중입니다!');
+        return;
       }}
-      href={href}
+      href={'/'}
       className={`flex h-12 w-full items-center justify-center rounded-lg text-body1 text-black disabled:bg-gray-100 disabled:text-gray-500 ${styles}`}
     >
       <div className="flex items-center justify-center">{icon}</div>
