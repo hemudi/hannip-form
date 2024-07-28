5e364f4b286a600d20733405ea58123bd7149477
Hemudi <ksum1205@naver.com>
2024-07-28 18:25:55 +0900

    :bug: Http 에서 스크립트 복사 기능 추가

--- src/utils/copyText.tsx
+++ src/utils/copyText.tsx
@@ -1,6 +1,29 @@
 import { toast } from 'react-toastify';
 
+const HTTP_PROTOCOL = 'http:';
+
 const copyText = async (text: string) => {
+  const protocol = document.location.protocol;
+
+  if (protocol === HTTP_PROTOCOL) {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    textArea.setSelectionRange(0, 99999);
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      toast.dismiss();
+      toast.success('스크립트 복사에 실패했습니다!');
+    }
+    textArea.setSelectionRange(0, 0);
+    document.body.removeChild(textArea);
+    toast.dismiss();
+    toast.success('스크립트가 복사되었습니다!');
+    return;
+  }
+
   const canUseShareApi =
     typeof window !== 'undefined' &&
     window.navigator.canShare &&
