2761a2e82d263e390e86888bcd75b88edcbd89df
Hemudi <ksum1205@naver.com>
2024-08-12 22:28:51 +0900

    :sparkles: Naver Login 수정

--- src/components/Page/Home/LoginButton/index.tsx
+++ src/components/Page/Home/LoginButton/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Icon from '@components/common/Icon';
-import Link from 'next/link';
 import { toast } from 'react-toastify';
 
 interface LoginButtonProps {
@@ -13,8 +12,7 @@ const buttonSettings = {
     icon: <Icon className="mt-1.5" type="kakao" />,
     text: '카카오 로그인',
     styles: 'bg-kakao text-black',
-    // href: `${process.env.NEXT_PUBLIC_API_FE_URL}/auth/kakao/callback`,
-    href: `/`,
+    href: `${process.env.NEXT_PUBLIC_API_FE_URL}/auth/kakao/callback`,
   },
   naver: {
     icon: <Icon className="mt-1.5" type="naver" color="#ffffff" />,
@@ -25,16 +23,17 @@ const buttonSettings = {
 };
 
 const LoginButton = ({ type }: LoginButtonProps) => {
-  const { icon, text, styles } = buttonSettings[type];
+  const { icon, text, styles, href } = buttonSettings[type];
   return (
     <a
       onClick={() => {
         if (type === 'naver') {
           toast.dismiss();
           toast.success('네이버 로그인 기능은 준비중입니다!');
+          return;
         }
       }}
-      href={`${process.env.NEXT_PUBLIC_API_FE_URL}/auth/kakao/callback`}
+      href={href}
       className={`flex h-12 w-full items-center justify-center gap-2 rounded-lg text-body1 disabled:bg-gray-100 disabled:text-gray-500 ${styles}`}
     >
       <div className="flex items-center justify-center">{icon}</div>
