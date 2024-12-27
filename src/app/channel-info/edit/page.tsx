a8a4b9a5bed34ed7c6dfb8bf436e3bc0d1255a4b
Hemudi <ksum1205@naver.com>
2024-12-27 20:28:24 +0900

    :sparkles: 채널 정보 수정 페이지 조건부 렌더링 제거

--- src/app/channel-info/edit/page.tsx
+++ src/app/channel-info/edit/page.tsx
@@ -56,31 +56,28 @@ const ChannelInfo = () => {
         }
       />
       <Layout.Main>
-        {description !== '' && category !== '' && (
-          <div className="flex h-full w-full flex-col items-center gap-8">
-            <div className={`flex w-full flex-col gap-2`}>
-              <div className="w-full text-body1 font-semibold text-gray-900">
-                채널을 소개해주세요<span className="text-gray-400"> *</span>
-              </div>
-              <TextArea
-                defaultValue={description}
-                placeholder="예시) 30대 여자의 뚜벅이 제주도 한달살이 브이로그를 공유하는 채널입니다."
-                onChange={handleOnChange}
-              />
+        <div className="flex h-full w-full flex-col items-center gap-8">
+          <div className={`flex w-full flex-col gap-2`}>
+            <div className="w-full text-body1 font-semibold text-gray-900">
+              채널을 소개해주세요<span className="text-gray-400"> *</span>
             </div>
-            <div className={`flex w-full flex-col gap-2`}>
-              <div className="w-full text-body1 font-semibold text-gray-900">
-                만들고 싶은 영상의 카테고리를 알려주세요<span className="text-gray-400"> *</span>
-              </div>
-              <Dropdown
-                defaultOption={{ value: category, label: category }}
-                placeholder="카테고리를 선택해주세요"
-                options={categoryList.map((category) => ({ value: category, label: category }))}
-                handleOptionChange={handleOptionChange}
-              />
-            </div>
+            <TextArea
+              defaultValue={description}
+              placeholder="예시) 30대 여자의 뚜벅이 제주도 한달살이 브이로그를 공유하는 채널입니다."
+              onChange={handleOnChange}
+            />
           </div>
-        )}
+          <div className={`flex w-full flex-col gap-2`}>
+            <div className="w-full text-body1 font-semibold text-gray-900">
+              만들고 싶은 영상의 카테고리를 알려주세요<span className="text-gray-400"> *</span>
+            </div>
+            <Dropdown
+              placeholder="카테고리를 선택해주세요"
+              options={categoryList.map((category) => ({ value: category, label: category }))}
+              handleOptionChange={handleOptionChange}
+            />
+          </div>
+        </div>
       </Layout.Main>
       <Layout.BottomMenu>
         <Button onClick={saveChannelInfo} disabled={category === '' || description === ''}>
