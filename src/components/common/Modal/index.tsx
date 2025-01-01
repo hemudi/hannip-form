3e323753f177b5c27064c2f8b37f764e9da4c842
Hemudi <ksum1205@naver.com>
2025-01-02 03:05:12 +0900

    :sparkles: 스크립트 생성 팁 모달 구현

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,10 +1,10 @@
+import { ReactNode } from 'react';
 import Icon from '@components/common/Icon';
-import { ReactNode, useState } from 'react';
 
 interface ModalProps {
-  title: string;
+  title?: string;
   children: ReactNode;
-  onClose: () => void;
+  onClose?: () => void;
 }
 
 const ModalContainer = ({ children }: { children: ReactNode }) => {
@@ -18,12 +18,16 @@ const ModalContainer = ({ children }: { children: ReactNode }) => {
 const Modal = ({ title, children, onClose }: ModalProps) => {
   return (
     <ModalContainer>
-      <div className="flex w-89 flex-col items-center justify-center gap-6 rounded-lg bg-white p-5 text-black shadow">
+      <div className="flex h-full w-89 flex-col items-center justify-center gap-6 rounded-lg bg-white p-5 text-black shadow">
         <div className="flex w-full flex-col items-end gap-1">
-          <div className="cursor-pointer" onClick={onClose}>
-            <Icon type="closeCross" />
-          </div>
-          <div className="w-full whitespace-pre-line text-center text-h4 font-bold">{title}</div>
+          {onClose && (
+            <div className="cursor-pointer" onClick={onClose}>
+              <Icon type="closeCross" />
+            </div>
+          )}
+          {title && (
+            <div className="w-full whitespace-pre-line text-center text-h4 font-bold">{title}</div>
+          )}
         </div>
         {children}
       </div>
