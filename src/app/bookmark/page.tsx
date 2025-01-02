49af4d7977eacc49ede074b3da968581d00793a3
Hemudi <ksum1205@naver.com>
2025-01-02 19:52:03 +0900

    :bug: Bookmark 페이지 useSearchParams missing suspense 해결

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import BookmarkList from '@components/Page/MyPage/BookmarkList';
 import UserInfo from '@components/Page/MyPage/UserInfo';
+import { Suspense } from 'react';
 
 const BookmarkPage = () => {
   return (
     <div className="h-full w-full">
       <UserInfo />
-      <BookmarkList />
+      <Suspense>
+        <BookmarkList />
+      </Suspense>
     </div>
   );
 };
