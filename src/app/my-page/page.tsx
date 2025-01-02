78881f070840d9b54b62296ff9d50dcc2958ef23
Hemudi <ksum1205@naver.com>
2025-01-02 17:55:13 +0900

    :sparkles: UserInfo 컴포넌트 유서 데이터 내부 상태로 변경

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -3,15 +3,15 @@
 import Icon from '@components/common/Icon';
 import MenuLayout from '@components/Page/MyPage/MenuLayout';
 import { MY_PAGE_MENUS } from '@components/Page/MyPage/MenuLayout/constants';
-import UserInfo, { UserInfoData } from '@components/Page/MyPage/UserInfo';
+import UserInfo from '@components/Page/MyPage/UserInfo';
 import { ROUTING_PATH } from '@constants/routingPath';
 import LoginModal from '@components/Page/Login/LoginModal';
 import Link from 'next/link';
-import { MouseEvent, useEffect, useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import { deleteCookie, getCookie } from 'cookies-next';
 import { COOKIE_NAME } from '@constants/cookieName';
 import { useRouter } from 'next/navigation';
-import { deleteUser, getUser } from '@apis/user';
+import { deleteUser } from '@apis/user';
 import { toast } from 'react-toastify';
 import Modal from '@components/common/Modal';
 import Button from '@components/common/Button';
@@ -31,7 +31,6 @@ interface UserModalState {
 const MyPage = () => {
   const [isShow, setIsShow] = useState<boolean>(false);
   const [warningText, setWarningText] = useState<string>('');
-  const [userInfo, setUserInfo] = useState<UserInfoData>();
   const [{ type, isModalShow }, setModalState] = useState<UserModalState>({
     type: '로그아웃',
     isModalShow: false,
@@ -39,14 +38,6 @@ const MyPage = () => {
 
   const isLogin = getCookie(COOKIE_NAME.ACCESS);
   const router = useRouter();
-
-  useEffect(() => {
-    if (isLogin) {
-      getUser().then((data) => {
-        setUserInfo(data);
-      });
-    }
-  }, []);
 
   const clickModal = (type: string) =>
     setModalState(({ isModalShow }) => ({ type, isModalShow: !isModalShow }));
@@ -76,7 +67,7 @@ const MyPage = () => {
 
   return (
     <div className="h-full w-full pt-10">
-      {isLogin ? userInfo && <UserInfo {...userInfo} /> : <NoUserInfo />}
+      {isLogin ? <UserInfo /> : <NoUserInfo />}
       <div className="h-full w-full bg-gray-50">
         <Link
           onClick={handleRequireLogin('채널정보는')}
