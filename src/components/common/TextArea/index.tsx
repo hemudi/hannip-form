b3e1b509a8292e0ca8a85c5ff10a7b98da4cd8d7
Hemudi <ksum1205@naver.com>
2025-01-26 00:38:07 +0900

    :sparkles: Form의 validation을  onChange에서 onBlur 로 변경

--- src/components/common/TextArea/index.tsx
+++ src/components/common/TextArea/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ComponentPropsWithoutRef, useEffect, useState } from 'react';
+import { ChangeEvent, FocusEvent, ComponentPropsWithoutRef, useState } from 'react';
 
 interface TextAreaProps extends ComponentPropsWithoutRef<'textarea'> {
   variant?: keyof typeof variantStyle;
@@ -22,19 +22,31 @@ const TextArea = ({
   ...props
 }: TextAreaProps) => {
   const [isError, setIsError] = useState<boolean>(false);
+  const [value, setValue] = useState<string>(defaultValue);
 
-  const handleOnChangeWithValidation = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  const handleOnChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
     if (onChange) onChange(e);
-    if (!validateValue) return;
-    setIsError(!validateValue(e.target.value));
+  };
+
+  const handleOnBlur = (e: FocusEvent<HTMLTextAreaElement>) => {
+    if (validateValue) {
+      setIsError(!validateValue(e.target.value));
+    }
+  };
+
+  const handleOnFocus = () => {
+    setIsError(false);
   };
 
   return (
     <div className="flex h-fit w-full flex-col gap-1">
       <textarea
         className={`${isError ? variantStyle.error : variantStyle.default} box-border h-47 w-full resize-none rounded-lg border p-5 text-body1 text-black placeholder:text-gray-500 enabled:focus:border enabled:focus:bg-white enabled:focus:outline-none disabled:text-gray-500`}
-        defaultValue={defaultValue}
-        onChange={handleOnChangeWithValidation}
+        value={value}
+        onChange={handleOnChange}
+        onBlur={handleOnBlur}
+        onFocus={handleOnFocus}
         {...props}
       />
       {isError ? (
--- src/components/common/TextField/index.tsx
+++ src/components/common/TextField/index.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, ComponentPropsWithoutRef, useState } from 'react';
+import { ChangeEvent, FocusEvent, ComponentPropsWithoutRef, useState } from 'react';
 
 interface TextFieldProps extends ComponentPropsWithoutRef<'input'> {
   variant?: keyof typeof variantStyle;
   helperMsg?: string;
   errorMsg?: string;
+  defaultValue?: string;
   validateValue?: (value: string) => boolean;
 }
 
@@ -22,19 +23,31 @@ const TextField = ({
   ...props
 }: TextFieldProps) => {
   const [isError, setIsError] = useState<boolean>(false);
+  const [value, setValue] = useState<string>(defaultValue);
 
-  const handleOnChangeWithValidation = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
     if (onChange) onChange(e);
-    if (!validateValue) return;
-    setIsError(!validateValue(e.target.value));
+  };
+
+  const handleOnBlur = (e: FocusEvent<HTMLInputElement>) => {
+    if (validateValue) {
+      setIsError(!validateValue(e.target.value));
+    }
+  };
+
+  const handleOnFocus = () => {
+    setIsError(false);
   };
 
   return (
     <div className="flex h-fit w-full flex-col gap-1">
       <input
         className={`${isError ? variantStyle.error : variantStyle.default} box-border h-12 w-full rounded-lg border px-4 text-body1 text-black placeholder:text-gray-500 enabled:focus:border enabled:focus:bg-white enabled:focus:outline-none disabled:text-gray-500`}
-        defaultValue={defaultValue}
-        onChange={handleOnChangeWithValidation}
+        value={value}
+        onChange={handleOnChange}
+        onBlur={handleOnBlur}
+        onFocus={handleOnFocus}
         {...props}
       />
       {isError ? (
