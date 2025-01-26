890835990000a8faeba38be3269d0e8ee3e8dab6
Hemudi <ksum1205@naver.com>
2025-01-27 08:06:24 +0900

    :sparkles: 로그인 후 이전 페이지로 돌아가는 기능 구현

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,25 +3,42 @@
 import { checkChannelInfo } from '@apis/user';
 import { COOKIE_NAME } from '@constants/cookieName';
 import { ROUTING_PATH } from '@constants/routingPath';
+import { HISTORY_PATH } from '@hooks/usePathHistory';
 import { setCookie } from 'cookies-next';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const AuthCallbackPage = () => {
+  const [prevPath, setPrevPath] = useState<string>();
   const searchParams = useSearchParams();
   const router = useRouter();
   const code = searchParams.get('code');
 
   useEffect(() => {
     setCookie(COOKIE_NAME.ACCESS, code);
-    checkChannelInfo().then((isExist) => {
-      if (isExist) {
-        router.replace(ROUTING_PATH.MAIN);
-      } else {
-        router.replace(ROUTING_PATH.CHANNEL_INFO);
-      }
-    });
+    const storage = globalThis?.sessionStorage;
+    if (storage) {
+      const storedPrevPath = storage.getItem(HISTORY_PATH.PREV);
+      setPrevPath(storedPrevPath || ROUTING_PATH.MAIN);
+    }
   }, []);
+
+  useEffect(() => {
+    if (!prevPath) return;
+
+    if (prevPath === ROUTING_PATH.LOGIN) {
+      checkChannelInfo().then((isExist) => {
+        if (isExist) {
+          router.replace(ROUTING_PATH.MAIN);
+        } else {
+          router.replace(ROUTING_PATH.CHANNEL_INFO);
+        }
+      });
+      return;
+    }
+
+    router.replace(prevPath);
+  }, [prevPath]);
 
   return null;
 };
