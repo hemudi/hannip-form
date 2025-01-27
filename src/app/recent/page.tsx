18386369a8fefd5dfe0cc17480c3d56b176d4c03
Hemudi <ksum1205@naver.com>
2025-01-27 11:00:50 +0900

    :bug: Suspense 추가

--- src/app/recent/page.tsx
+++ src/app/recent/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import RecentCreationList from '@components/Page/Recent/RecentCreationList';
+import { Suspense } from 'react';
 
 const RecentPage = () => {
   return (
-    <>
+    <Suspense>
       <RecentCreationList />
-    </>
+    </Suspense>
   );
 };
 
